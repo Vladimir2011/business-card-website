4b9185e831c432c8a42f756bea2877d755c4aac8
Vladimir Garbuzov <vladimir.garbuzzov@yandex.ru>
2022-05-12 22:49:01 +0300

    create meeting component, write styles for him

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,24 @@
 <template>
-  <Header />
-  <main role="main">
-    <div class="container">
-      <section class="meeting">
-        <div class="meeting__container">
-          <div class="meeting__info-block">
-            <p class="meeting__paragraph">Hello world ... I am</p>
-            <h1 class="meeting__header">Гарбузов Владимир</h1>
-            <p class="meeting__profession-name"> > Front-end developer</p>
-            <ul class="meeting__comments-list">
-              <li class="meeting__comments-item">// знаю, что выгляжу на фото как инфоцыган</li>
-              <li class="meeting__comments-item">// не представляю жизни без кофе</li>
-              <li class="meeting__comments-item meeting__comments-item--variable">
-                <span class="meeting__comments-item-span-link">const githubLink</span> = <span class="meeting__comments-item-span-link">"https://github.com/Vladimir2011"</span>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </section>
-    </div>
-  </main>
-  <Footer />
+  <div class="wrapper" id="#app">
+    <Header />
+    <main role="main">
+      <Meeting />
+      <div class="container">
+
+      </div>
+    </main>
+    <Footer />
+  </div>
 </template>
 
 <script>
 import Header from '@/components/Header'
 import Footer from '@/components/Footer'
+import Meeting from '@/components/Meeting'
 import { app } from '@/_config'
 
 export default {
-  components: { Header, Footer },
+  components: { Header, Footer, Meeting },
   setup () {
     const title = app.title
     document.title = title
--- src/_config.js
+++ src/_config.js
@@ -1,11 +1,3 @@
-export const process = {
-  dev: true
-}
-
-// export const site = {
-//   home: process.dev ? 'http://localhost:8080/' : 'https://tocode.ru'
-// }
-
 export const app = {
   title: 'Title майтл'
 }

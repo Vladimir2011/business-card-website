3e603243430787e2f3fe666f205a612978779296
Vladimir Garbuzov <vladimir.garbuzzov@yandex.ru>
2022-05-18 14:58:26 +0300

    complete tablet version

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,7 +1,28 @@
 <template>
   <header class="header">
-    <div class="header__container">
-      <p class="header__logo-text">Frontend-developer</p>
+    <!--Mobile menu start-->
+    <div class="header__container header__container--adaptive">
+      <p class="header__logo-text">
+        Frontend-developer
+      </p>
+      <Button
+        :name="''"
+        :class-name="'header__burger-button'"
+        @button-click="isMenuOpen = true"
+      />
+    </div>
+    <!--Mobile menu end-->
+    <div class="header__container" :class="{'header__container--open' : isMenuOpen}">
+      <p class="header__logo-text">
+        Frontend-developer
+        <span class="header__close" @click="isMenuOpen = false">
+          <img
+            src="@/static/svg/close-menu-icon.svg"
+            alt="icon"
+            class="header__close-icon"
+          >
+        </span>
+      </p>
       <nav class="header__nav nav">
         <ul class="nav__list">
           <li
@@ -17,24 +38,26 @@
         <button class="localization__button localization__button-active">RU</button>
         <button class="localization__button">ENG</button>
       </div>
-
-      <!--Mobile menu-->
-
-      <!--Mobile menu end-->
     </div>
   </header>
 </template>
 
 <script>
+import Button from '@/components/Button'
 import { links } from '@/_config'
 import { ref } from "vue";
 
 export default {
+  components: {
+    Button
+  },
   setup () {
     const linksList = ref(links)
+    const isMenuOpen = ref(true)
 
     return {
-      linksList
+      linksList,
+      isMenuOpen
     }
   }
 }

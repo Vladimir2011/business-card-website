d7d26cf09cc52f7ed45681c753d6d27c75a14946
Vladimir Garbuzov <vladimir.garbuzzov@yandex.ru>
2022-05-22 21:26:40 +0300

    remove meta

--- src/main.js
+++ src/main.js
@@ -1,12 +1,9 @@
 import { createApp } from 'vue'
-import VueMeta from 'vue-meta'
 import App from './App.vue'
 
 import './assets/scss/main.scss'
 import 'animate.css'
 
 const app = createApp(App)
-
-app.use(VueMeta)
 
 app.mount('#app')

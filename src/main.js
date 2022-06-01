3cb53ca5f771f3af3bba0b8adea50b9572c76ed6
Vladimir Garbuzov <vladimir.garbuzzov@yandex.ru>
2022-06-01 10:01:25 +0300

    add google analytics

--- src/main.js
+++ src/main.js
@@ -1,9 +1,13 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import VueGtag from "vue-gtag";
 
 import './assets/scss/main.scss'
 import 'animate.css'
 
 const app = createApp(App)
+app.use(VueGtag, {
+  config: { id: 'G-CL3EYX1FEN' }
+})
 
 app.mount('#app')
